37b3facdffde0f8b9abab8e2a9ac5a31803c63c0
Loschinskey <zloschin@stjude.org>
2022-06-28 11:31:53 -0500

    Added Comparagrams

--- Histograms.r
+++ Histograms.r
@@ -25,6 +25,47 @@ plotDistribution = function (x, title, xlabel, ylabel, filenam, breks) {
   dev.off()
 }
 
+plotCompareDistribution = function (x, y, title, xlabel, ylabel, filenam, breks){
+  # Define transparent colors for easy visualization
+  myBlue <- rgb(0, 0, 255, max = 255, alpha = 64, names = "blue25")
+  myRed <- rgb(255, 0, 0, max = 255, alpha = 64, names = "red25")
+  
+  # Get rid of missing values
+  x <- na.omit(x)
+  y <- na.omit(y)
+  
+  # Create file for saving
+  png(filename=filenam)
+  
+  # Plot first histogram with given parameters
+  hist(x,col = myBlue,
+      main = title,
+      xlab=xlabel,
+      ylab=ylabel,
+      xlim=c(0,1),
+      ylim=c(0,80),
+      breaks=breks)
+  # Plot line
+  lines(density(x), col="blue", lwd=3)
+  
+  # Plot second histogram with given parameters
+  hist(y,col=myRed,
+       main = title,
+       xlab=xlabel,
+       ylab=ylabel,
+       xlim=c(0,1),
+       ylim=c(0,80),
+       breaks=breks,
+       add=TRUE)
+  # Plot line
+  lines(density(y), col="red", lwd=3)
+  legend("topright", c("Case", "Control"), fill=c("red", "blue"))
+  
+  # Clear figure
+  dev.off()
+  
+}
+
 # Import library to read Excel files
 library("readxl")
 
@@ -54,7 +95,7 @@ for(i in 3:7){
 }
 
 # Create histograms for percent time in each state
-for(i in 7:ncol(my_data)){
+for(i in 8:12){
   # Prepare inputs for dist function
   toPlot = as.numeric(unlist(my_data[ , i]))
   state = unlist(c(strsplit(name[i], split="_")))[1]
@@ -66,5 +107,36 @@ for(i in 7:ncol(my_data)){
   
   # Call my distribution function
   plotDistribution(toPlot, title, xLabel, yLabel, filename, breaks)
+}
+
+# Import data for comparison charts
+casePath = "Z:\\ResearchHome\\ClusterHome\\zloschin\\Spindle_Percent_Data\\Case_Percent_Data.xlsx"
+controlPath = "Z:\\ResearchHome\\ClusterHome\\zloschin\\Spindle_Percent_Data\\Control_Percent_Data.xlsx"
+caseData = read_excel(casePath)
+controlData = read_excel(controlPath)
+
+# Set up path for plot storage
+histpath = "C:\\Users\\zloschin\\Documents\\APNEA_Project\\Comparagrams"
+
+# Create comparagrams for each sleep state
+name = names(caseData)
+
+for(i in 3:7){
+  # Define data to plot
+  toPlotCase = as.numeric(unlist(caseData[ , i]))
+  toPlotControl = as.numeric(unlist(my_data[ , i]))
+  
+  # Get the state name
+  state = unlist(c(strsplit(name[i], split="_")))[1]
+  
+  # Define graphing parameters
+  title = paste("Comparison of", state, "btwn Cases and Controls")
+  xLabel = paste("Fraction of spindles in", state, "State")
+  yLabel = "Number of Patients"
+  filename = paste(histpath, "\\Comp_", name[i], ".png", sep="")
+  breaks = 25
+  
+  # Call plot function
+  plotCompareDistribution(x=toPlotControl, y=toPlotCase, xlabel=xLabel, ylabel = yLabel, title=title, filenam = filename, breks=breaks)
 }
 
