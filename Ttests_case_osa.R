ee96372d110017a2b8f8b3e4d594e3d109d82728
Loschinskey <zloschin@stjude.org>
2022-07-05 14:30:52 -0500

    T-Tests for Osa vs Nonosa in case and control populations

--- Ttests_case_osa.R
+++ Ttests_case_osa.R
@@ -72,38 +72,10 @@ plotCompareDistribution = function (x, y, title, xlabel, ylabel, filenam, breks)
 # Import library to read Excel files
 library("readxl")
 
-# Set up path to excel files
-datapath = "C:\\Users\\zloschin\\Excel_Data_All\\Percentage_Data_Subset2.xlsx"
 
 # Set up path for plot storage
-histpath = "C:\\Users\\zloschin\\Documents\\APNEA_Project\\Histograms"
+histpath = "C:\\Users\\zloschin\\Documents\\APNEA_Project\\T_Test_Graphs"
 
-# Import data
-my_data <- read_excel(path = datapath)
-
-my_data2 = as.data.frame(my_data)
-
-# Create histograms for percent spindles in each state
-name = names(my_data)
-
-# Define list of files
-file_list_cases <- list.files(path="Z:/ResearchHome/ClusterHome/zloschin/Spind_Data_Case_Controls/Cases_Excel_Data",
-                        full.names = TRUE,
-                        recursive = TRUE)
-
-freqValuesCases <- c()
-
-for(i in 1:length(file_list_cases)){
-  # Read in excel
-  tempDF <- read_excel(file_list_cases[i])
-  tempDF <- as.data.frame(tempDF)
-  tempDF2 <- tempDF[tempDF$State == 1 | tempDF$State == 0 , ]
-  tempDF3 <- tempDF2[tempDF2$Frequency <= 12.5 , ]
-  
-  freqs <- mean(tempDF3$Frequency, na.rn=TRUE)
-  freqValuesCases[i] = freqs
-  
-}
 
 # Define list of files
 file_list_cont <- list.files(path="Z:/ResearchHome/ClusterHome/zloschin/Spind_Data_Case_Controls/Controls_Excel_Data",
@@ -128,8 +100,7 @@ for(i in 1:length(file_list_cont)){
 testResult = t.test(freqValuesCases, freqValuesControls)
 
 
-# OSA vs non-OSA comparison
-
+# Cases: OSA vs non-OSA comparison
 
 
 # Define list of files
@@ -171,16 +142,105 @@ for(i in 1:length(file_list_nonosa)){
 
 testResult = t.test(freqValuesOSA, freqValuesnonosa, var.equal=TRUE )
 
-plotCompareDistribution(x = freqValuesOSA, y = freqValuesnonosa, 
-                        xlabel="Frequency (Hz)", ylabel = "Distribution", 
-                        filenam="test.png", breks = 25, title = "Dist")
+
+# Define plot colors
+myBlue <- rgb(0, 0, 255, max = 255, alpha = 50, names = "blue25")
+myRed <- rgb(255, 0, 0, max = 255, alpha = 50, names = "red25")
+
+png("C:\\Users\\zloschin\\Documents\\APNEA_Project\\T_Test_Graphs\\Cases_Osa_vs_nonOSA.png")
+freqValuesOSA = na.omit(freqValuesOSA)
+freqValuesnonosa= na.omit(freqValuesnonosa)
+hist(freqValuesOSA, col=myRed, breaks=20, xlab="Spindle Frequency", 
+     ylab="Number of Patients",
+     main = paste("Cases: Osa Vs NonOSA Comparison \nT-test P-val =", 
+                  round(testResult$p.value, digits=3), "\nDF =", 
+                  testResult$parameter))
+
+lines(density(freqValuesOSA), col='red')
+
+hist(freqValuesnonosa, col=myBlue, breaks=20, xlab="Spindle Frequency", 
+     ylab="Number of Patients",
+     main = paste("Cases: Osa Vs NonOSA Comparison \nT-test P-val =", 
+                  round(testResult$p.value, digits=3), "\nDF =", 
+                  testResult$parameter), add=TRUE)
+
+lines(density(freqValuesnonosa), col='blue')
+
+dev.off()
 
 
-# png(filename = "test.png")
-# freqValuesOSA = na.omit(freqValuesOSA)
-# hist(freqValuesOSA)
-# lines(density(freqValuesOSA))
-# 
-# dev.off()
+
+
+# Controls: OSA vs nonOSA comparison
+
+
+
+# Define list of files
+file_list_osa <- list.files(path="Z:\\ResearchHome\\ClusterHome\\zloschin\\Spindle_Data_Controls\\Control_OSA_Excel_Data",
+                            full.names = TRUE,
+                            recursive = TRUE)
+
+freqValuesOSA <- c()
+
+for(i in 1:length(file_list_osa)){
+  # Read in excel
+  tempDF <- read_excel(file_list_osa[i])
+  tempDF <- as.data.frame(tempDF)
+  tempDF2 <- tempDF[tempDF$State == 1 | tempDF$State == 0 , ]
+  tempDF4 <- tempDF2[tempDF2$Frequency <= 12.5 , ]
+  
+  freqs <- mean(tempDF4$Frequency, na.rn=TRUE)
+  freqValuesOSA = append(freqValuesOSA, freqs)
+}
+
+# Define list of files for nonOSA
+file_list_nonosa <- list.files(path="Z:\\ResearchHome\\ClusterHome\\zloschin\\Spindle_Data_Controls\\Control_NonOSA_Excel_Data",
+                               full.names = TRUE,
+                               recursive = TRUE)
+
+freqValuesnonosa <- c()
+
+for(i in 1:length(file_list_nonosa)){
+  # Read in excel
+  tempDF <- read_excel(file_list_nonosa[i])
+  tempDF <- as.data.frame(tempDF)
+  tempDF2 <- tempDF[tempDF$State == 1 | tempDF$State == 0 , ]
+  tempDF3 <- tempDF2[tempDF2$Frequency <= 12.5 , ]
+  
+  freqs <- mean(tempDF3$Frequency, na.rn=TRUE)
+  freqValuesnonosa = append(freqValuesnonosa, freqs)
+}
+
+
+testResult = t.test(freqValuesOSA, freqValuesnonosa, var.equal=TRUE )
+
+
+# Define plot colors
+myBlue <- rgb(0, 0, 255, max = 255, alpha = 50, names = "blue25")
+myRed <- rgb(255, 0, 0, max = 255, alpha = 50, names = "red25")
+
+png("C:\\Users\\zloschin\\Documents\\APNEA_Project\\T_Test_Graphs\\Controls_Osa_vs_nonOSA.png")
+freqValuesOSA = na.omit(freqValuesOSA)
+freqValuesnonosa= na.omit(freqValuesnonosa)
+hist(freqValuesOSA, col=myRed, breaks=25, xlab="Spindle Frequency", 
+     ylab="Number of Patients",
+     main = paste("Controls: Osa Vs NonOSA Comparison \nT-test P-val =", 
+                  round(testResult$p.value, digits=3), 
+                  "\n", "DF =", testResult$parameter))
+
+lines(density(freqValuesOSA), col='red')
+
+hist(freqValuesnonosa, col=myBlue, breaks=25, xlab="Spindle Frequency", 
+     ylab="Number of Patients",
+     main = paste("Controls: Osa Vs NonOSA Comparison \nT-test P-val =", 
+                  round(testResult$p.value, digits=3), 
+                  "\n", "DF =", testResult$parameter), add=TRUE)
+
+lines(density(freqValuesnonosa), col='blue')
+
+dev.off()
+
+
+
 
 
