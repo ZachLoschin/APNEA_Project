0fc854aafc155479d2d6352f765c3bf33d3187f6
Zachloschin <zachloschin@gmail.com>
2022-07-19 16:52:56 -0500

    Pipeline 1 Ready

--- Official_Runs_Folder/Pipeline_1_Hypnograms.py
+++ Official_Runs_Folder/Pipeline_1_Hypnograms.py
@@ -1,11 +1,9 @@
 import pathlib
 import My_Funcs as my
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import yasa
 import mne
-import scipy.signal
 
 """
 Zachary Loschinskey
@@ -57,22 +55,23 @@ print("Creating directories...")
 
 """
 # Data directory for official runs
-data_dir = pathlib.Path("Z:/ResearchHome/ClusterHome/asanch24/APNEA/APNEA Raw Files/AllEDFs/")
+data_dir = pathlib.Path("Z:/ResearchHome/ClusterHome/asanch24/APNEA/APNEA Raw Files/Analysis_EDFs/")
 
 # Directory for official running
-block3dir = pathlib.Path("Z:/ResearchHome/ClusterHome/asanch24/APNEA/Block3_51/")
+block3dir = pathlib.Path("Z:/ResearchHome/ClusterHome/asanch24/APNEA/Pipeline1/")
 block3dir.mkdir(parents=True, exist_ok=True)
 
 # Shared directory for official runs
-dict_dir = pathlib.Path("Z:/ResearchHome/ClusterHome/asanch24/APNEA/Block3_51/Excel_Data_All/")
+dict_dir = pathlib.Path("Z:/ResearchHome/ClusterHome/asanch24/APNEA/Pipeline1/Excel_Data_All/")
 dict_dir.mkdir(parents=True, exist_ok=True)
 
 # Plot directory for official runs
-dens_dir = pathlib.Path("Z:/ResearchHome/ClusterHome/asanch24/APNEA/Block3_51/Density_Hypno_Plots/")
+dens_dir = pathlib.Path("Z:/ResearchHome/ClusterHome/asanch24/APNEA/Pipeline1/Density_Hypno_Plots/")
+dens_dir.mkdir(parents=True)
 
 # Official error directory
-error_dir = pathlib.Path("Z:/ResearchHome/ClusterHome/asanch24/APNEA/Block3_51/Error_Storage/")
-error_dir.mkdir()
+error_dir = pathlib.Path("Z:/ResearchHome/ClusterHome/asanch24/APNEA/Pipeline1/Error_Storage/")
+error_dir.mkdir(parents=True)
 """
 
 # Local data folder for testing
@@ -91,7 +90,11 @@ data_dir = pathlib.Path("../Block3_Local/Data")
 files = pathlib.Path(data_dir).glob('*')
 
 # Local data for reading in night division data
-div_data = pathlib.Path("../Block3_Local/Rec_Times_50.xlsx")
+div_data = pathlib.Path("../Official_Runs_Folder/CompleteTable2.xlsx")
+
+# Local directory for error storage
+error_dir = pathlib.Path("../Block3_Local/Error_Storage")
+error_dir.mkdir(parents=True, exist_ok=True)
 
 # Read in division data to DataFrame
 DivDF = pd.read_excel(div_data)
@@ -112,129 +115,193 @@ NREM1PercN2 = []
 NREM2PercN2 = []
 NREM3PercN2 = []
 
+ID_List = DivDF["EDF_ID"]
+
 # For each .edf file
 for file in files:
 
     # Setup try for each file to not stop running if one error occurs
 
-    with open(file, 'rb') as f:
+    try:
+        with open(file, 'rb') as f:
 
-        """
-        1) Read in EDF
-        """
-        split_array = str(file).split('\\')
-        name = split_array[-1]
-        print("Importing data...")
-        rawImport = mne.io.read_raw_edf(file, preload=True)
+            """
+            1) Read in EDF
+            """
+            split_array = str(file).split('\\')
+            name = split_array[-1]
+            print("Importing data...")
+            name = name.split('.')[0]
+            print(name)
+            ID = my.get_ID(file)
+            print(ID)
 
-        name = name.split('.')[0]
-        print(name)
-        ID = my.get_ID(file)
-        print(ID)
+            rawImport = mne.io.read_raw_edf(file, preload=True)
 
-        eeg = rawImport["EEG"][0][0]
-        filteredData = rawImport.filter(l_freq=None, h_freq=18, picks=["EEG"], method='iir')
+            eeg = rawImport["EEG"][0][0]
+            filteredData = rawImport.filter(l_freq=None, h_freq=18, picks=["EEG"], method='iir')
 
-        """
-        2) Use recording times to find where to split data
-        """
-        # Get the recording times for night 1 and 2
-        DivDF_Patient = DivDF.loc[DivDF["EDF_ID"] == ID]
+            """
+            2) Use recording times to find where to split data
+            """
+            # Get the recording times for night 1 and 2
+            DivDF_Patient = DivDF.loc[DivDF["EDF_ID"] == ID]
 
-        Time_N1 = float(DivDF_Patient.iloc[0]["Rec_Time_N1"][0:3])  # in hours
-        Time_N2 = float(DivDF_Patient.iloc[0]["Rec_Time_N2"][0:3])  # in hours
+            # Find how many nights there is data for
+            if len(DivDF_Patient) == 0:
+                nights = 0
+            elif len(DivDF_Patient) == 1:
+                nights = 1
+            elif len(DivDF_Patient) == 2:
+                nights = 2
+            else:
+                # Save -1 if there is an unexpected number of nights
+                nights = -1
 
-        # Convert these recording times to seconds then to samples
-        sample_n1 = (Time_N1 * 3600) * rawImport.info["sfreq"]
-        sample_n2 = (Time_N2 * 3600) * rawImport.info["sfreq"]
+            if nights == 0:
+                print("no nights")
+                continue
+            elif nights == -1:
+                continue
+            elif nights == 1:
+                """
+                4) Compute hypnograms for the one night
+                """
+                Night1_sls = yasa.SleepStaging(raw=rawImport, eeg_name="EEG", eog_name="LEOG")
 
-        # If there is left over in the middle, take midpoint
-        if sample_n2 + sample_n1 < len(eeg):
-            gap = len(eeg) - sample_n2 + sample_n1
+                # Get hypno objects
+                Night1_Hypno = Night1_sls.predict()
 
-            # Set sample one to include first half of gap
-            sample_n1 = sample_n1 + (0.5 * gap)
+                # Convert the hypnograms to numbers
+                Night1_HypnoEnum = my.hypno_to_plot_art(Night1_Hypno)
 
-            # Set sample 2 to include samples from middle of gap to end
-            sample_n2 = len(eeg) - sample_n1
+                """
+                5) Plot the hypnograms and save them to directory
+                """
+                # Create filenames for saving figures
+                Night1_File = "%s_Night1_Hypno.png" % ID
 
-        elif sample_n2 + sample_n1 > len(eeg):
+                # Create paths for saving figures
+                Night1_Path = dens_dir / Night1_File
 
-            # Calculate overlap
-            overlap = abs(len(eeg) - sample_n1 - sample_n2)
+                # Plot and save hypnogram for night 1
+                plt.plot(Night1_HypnoEnum)
+                plt.savefig(Night1_Path)
+                plt.clf()
 
-            # Set sample one to exclude half of gap
-            sample_n1 = sample_n1 - (0.5 * overlap)
+                """
+                6) Save percent time in each state
+                """
+                IDPerc.append(ID)
+                WakePercN1.append(Night1_HypnoEnum.count(4) / len(Night1_HypnoEnum))
+                REMPercN1.append(Night1_HypnoEnum.count(3) / len(Night1_HypnoEnum))
+                NREM1PercN1.append(Night1_HypnoEnum.count(2) / len(Night1_HypnoEnum))
+                NREM2PercN1.append(Night1_HypnoEnum.count(1) / len(Night1_HypnoEnum))
+                NREM3PercN1.append(Night1_HypnoEnum.count(0) / len(Night1_HypnoEnum))
 
-            # Set sample 2 to include samples from middle of overlap to end
-            sample_n2 = len(eeg) - sample_n1
+                WakePercN2.append(-1)
+                REMPercN2.append(-1)
+                NREM1PercN2.append(-1)
+                NREM2PercN2.append(-1)
+                NREM3PercN2.append(-1)
 
-        """
-        3) Split up by night
-        """
-        # Copy raw import to second one for cropping night 2
-        rawImport2 = rawImport.copy()
-        # Split the data into two nights
-        Night_1 = rawImport.crop(tmin=0, tmax=sample_n1 / rawImport.info["sfreq"], include_tmax=False)
-        Night_2 = rawImport2.crop(tmin=sample_n1 / rawImport.info["sfreq"],
-                                  tmax=(len(rawImport2) / rawImport.info["sfreq"]) - 60, include_tmax=False)
+            elif nights == 2:
+                Time_N1 = DivDF_Patient.iloc[0]["RecordingTime"]  # in hours
+                Time_N2 = DivDF_Patient.iloc[1]["RecordingTime"]  # in hours
 
-        """
-        4) Compute hypnograms for each night
-        """
-        # Calculate sleep stages using EEG and EOG channels on the filtered data
-        print("Sleep staging...")
-        Night1_sls = yasa.SleepStaging(raw=Night_1, eeg_name="EEG", eog_name="LEOG")
-        Night2_sls = yasa.SleepStaging(raw=Night_2, eeg_name="EEG", eog_name="LEOG")
+                # Convert these recording times to seconds then to samples
+                sample_n1 = (Time_N1 * 3600) * rawImport.info["sfreq"]
+                sample_n2 = (Time_N2 * 3600) * rawImport.info["sfreq"]
 
-        # Get hypno objects
-        Night1_Hypno = Night1_sls.predict()
-        Night2_Hypno = Night2_sls.predict()
+                # If there is left over in the middle, take midpoint
+                if sample_n2 + sample_n1 < len(eeg):
+                    gap = len(eeg) - sample_n2 + sample_n1
 
-        # Convert the hypnograms to numbers
-        Night1_HypnoEnum = my.hypno_to_plot_art(Night1_Hypno)
-        Night2_HypnoEnum = my.hypno_to_plot_art(Night2_Hypno)
+                    # Set sample one to include first half of gap
+                    sample_n1 = sample_n1 + (0.5 * gap)
 
-        """
-        5) Plot the hypnograms and save them to directory
-        """
-        # Create filenames for saving figures
-        Night1_File = "%s_Night1_Hypno.png" % ID
-        Night2_File = "%s_Night2_Hypno.png" % ID
+                    # Set sample 2 to include samples from middle of gap to end
+                    sample_n2 = len(eeg) - sample_n1
 
-        # Create paths for saving figures
-        Night1_Path = dens_dir / Night1_File
-        Night2_Path = dens_dir / Night2_File
+                elif sample_n2 + sample_n1 > len(eeg):
 
-        # Plot and save hypnogram for night 1
-        plt.plot(Night1_HypnoEnum)
+                    # Calculate overlap
+                    overlap = abs(len(eeg) - sample_n1 - sample_n2)
 
-        plt.savefig(Night1_Path)
-        plt.clf()
+                    # Set sample one to exclude half of gap
+                    sample_n1 = sample_n1 - (0.5 * overlap)
 
-        # Plot and save hypnogram for night 2
-        plt.plot(Night2_HypnoEnum)
+                    # Set sample 2 to include samples from middle of overlap to end
+                    sample_n2 = len(eeg) - sample_n1
 
-        plt.savefig(Night2_Path)
-        plt.clf()
-        print("Done")
+                """
+                3) Split up by night
+                """
+                # Copy raw import to second one for cropping night 2
+                rawImport2 = rawImport.copy()
+                # Split the data into two nights
+                Night_1 = rawImport.crop(tmin=0, tmax=sample_n1 / rawImport.info["sfreq"], include_tmax=False)
+                Night_2 = rawImport2.crop(tmin=sample_n1 / rawImport.info["sfreq"],
+                                          tmax=(len(rawImport2) / rawImport.info["sfreq"]) - 60, include_tmax=False)
 
-        """
-        6) Save percent time in each state
-        """
-        IDPerc.append(ID)
-        WakePercN1.append(Night1_HypnoEnum.count(4) / len(Night1_HypnoEnum))
-        REMPercN1.append(Night1_HypnoEnum.count(3) / len(Night1_HypnoEnum))
-        NREM1PercN1.append(Night1_HypnoEnum.count(2) / len(Night1_HypnoEnum))
-        NREM2PercN1.append(Night1_HypnoEnum.count(1) / len(Night1_HypnoEnum))
-        NREM3PercN1.append(Night1_HypnoEnum.count(0) / len(Night1_HypnoEnum))
+                """
+                4) Compute hypnograms for each night
+                """
+                # Calculate sleep stages using EEG and EOG channels on the filtered data
+                print("Sleep staging...")
+                Night1_sls = yasa.SleepStaging(raw=Night_1, eeg_name="EEG", eog_name="LEOG")
+                Night2_sls = yasa.SleepStaging(raw=Night_2, eeg_name="EEG", eog_name="LEOG")
 
-        WakePercN2.append(Night2_HypnoEnum.count(4) / len(Night2_HypnoEnum))
-        REMPercN2.append(Night2_HypnoEnum.count(3) / len(Night2_HypnoEnum))
-        NREM1PercN2.append(Night2_HypnoEnum.count(2) / len(Night2_HypnoEnum))
-        NREM2PercN2.append(Night2_HypnoEnum.count(1) / len(Night2_HypnoEnum))
-        NREM3PercN2.append(Night2_HypnoEnum.count(0) / len(Night2_HypnoEnum))
+                # Get hypno objects
+                Night1_Hypno = Night1_sls.predict()
+                Night2_Hypno = Night2_sls.predict()
 
+                # Convert the hypnograms to numbers
+                Night1_HypnoEnum = my.hypno_to_plot_art(Night1_Hypno)
+                Night2_HypnoEnum = my.hypno_to_plot_art(Night2_Hypno)
+
+                """
+                5) Plot the hypnograms and save them to directory
+                """
+                # Create filenames for saving figures
+                Night1_File = "%s_Night1_Hypno.png" % ID
+                Night2_File = "%s_Night2_Hypno.png" % ID
+
+                # Create paths for saving figures
+                Night1_Path = dens_dir / Night1_File
+                Night2_Path = dens_dir / Night2_File
+
+                # Plot and save hypnogram for night 1
+                plt.plot(Night1_HypnoEnum)
+
+                plt.savefig(Night1_Path)
+                plt.clf()
+
+                # Plot and save hypnogram for night 2
+                plt.plot(Night2_HypnoEnum)
+                plt.savefig(Night2_Path)
+                plt.clf()
+
+                """
+                6) Save percent time in each state
+                """
+                IDPerc.append(ID)
+                WakePercN1.append(Night1_HypnoEnum.count(4) / len(Night1_HypnoEnum))
+                REMPercN1.append(Night1_HypnoEnum.count(3) / len(Night1_HypnoEnum))
+                NREM1PercN1.append(Night1_HypnoEnum.count(2) / len(Night1_HypnoEnum))
+                NREM2PercN1.append(Night1_HypnoEnum.count(1) / len(Night1_HypnoEnum))
+                NREM3PercN1.append(Night1_HypnoEnum.count(0) / len(Night1_HypnoEnum))
+
+                WakePercN2.append(Night2_HypnoEnum.count(4) / len(Night2_HypnoEnum))
+                REMPercN2.append(Night2_HypnoEnum.count(3) / len(Night2_HypnoEnum))
+                NREM1PercN2.append(Night2_HypnoEnum.count(2) / len(Night2_HypnoEnum))
+                NREM2PercN2.append(Night2_HypnoEnum.count(1) / len(Night2_HypnoEnum))
+                NREM3PercN2.append(Night2_HypnoEnum.count(0) / len(Night2_HypnoEnum))
+    except:
+        error_file = '%s_%s.txt' % (ID, 'ErrorMSG')
+        error_path = error_dir / error_file
+        with open(error_path, 'w') as e:
+            e.write('Error running %s\n' % ID)
 
 # Outside patient loop
 StateTimes["IDs"] = IDPerc
