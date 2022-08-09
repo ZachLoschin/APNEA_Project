f46db82d36ef5e2cbfacbda10e2d9a35a88d9b1a
Catalina Saini <catalina.saini@uc.cl>
2022-08-09 18:33:36 -0400

    Update peak_calculation.py

--- peak_calculation.py
+++ peak_calculation.py
@@ -1,4 +1,5 @@
 from importlib.resources import files
+from PythonCode import My_Funcs as my
 import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,6 +14,7 @@ What this code does:
     2. Yasa spindle detection
     3. Artifact removal
     4. Filters: band pass filter and beta pass filter
+    5. ...
 '''
 
 '''
@@ -34,3 +36,229 @@ dens_dir.mkdir(parents=True, exist_ok=True)
 data_dir = pathlib.Path("../Apnea_Local/Data")
 files = pathlib.Path(data_dir).glob('*')
 
+# Local data for reading in night division data
+div_data = pathlib.Path("../Block3_Local/Rec_Times_50.xlsx")
+
+# Read in division data to DataFrame
+DivDF = pd.read_excel(div_data)
+
+# '''
+# Read archives (PSD_Confirmation.py)
+# '''
+
+# file_dir = pathlib.Path('Z:/ResearchHome/ClusterHome/asanch24/APNEA/APNEA Raw Files/Test_EDF/').glob('*')
+
+
+# for filename in file_dir:
+
+#     """
+#     2) Raw data import
+#     """
+#     split_array = str(filename).split('\\')
+#     name = split_array[-1]
+
+#     rawImport = mne.io.read_raw_edf(filename, preload=True)
+
+#     my.my_plot(rawImport["EEG"][0][0], rawImport["EEG"][1], "Raw Import Data", 1)
+
+#     win = int(4 * rawImport.info["sfreq"])
+
+#     freqs, psd = scipy.signal.welch(rawImport["EEG"][0][0], rawImport.info["sfreq"], nperseg=win)
+
+#     print("Plotting PSD")
+#     plt.clf()
+#     plt.plot(freqs, psd, 'k', lw=2)
+#     plt.fill_between(freqs, psd, cmap='Spectral')
+#     plt.xlim(1, 30)
+#     plt.yscale('log')
+#     plt.title('Raw Import PSD')
+#     plt.xlabel('Frequency (Hz)')
+#     plt.ylabel('PSD Log($uV^2$/Hz)')
+#     plt.show()
+
+'''
+Read archives (Raw data import)
+'''
+
+"""
+1) Raw data import
+"""
+split_array = str(file).split('\\')
+name = split_array[-1]
+print("Importing data...")
+rawImport = mne.io.read_raw_edf(file, preload=True)
+
+name = name.split('.')[0]
+print(name)
+ID = my.get_ID(file)
+print(ID)
+
+"""
+2) Creation of our data dictionary
+"""
+print("Creating dict...")
+# Create epoch vector
+epochVec = my.create_epochs(rawImport["EEG"][0][0], rawImport.info["sfreq"], 30)
+
+# Create epoch time vector
+epochTimeVec = my.create_epochs(rawImport["EEG"][1], rawImport.info["sfreq"], 30)
+
+# Create Dictionary for organization
+data = my.create_dict(rawImport["EEG"][0][0], rawImport.info["sfreq"], rawImport["EEG"][1], epochVec,
+                        epochTimeVec)
+
+# Add EOG channels to the dictionary
+data["REOG"] = rawImport["REOG"][0][0]
+data["LEOG"] = rawImport["LEOG"][0][0]
+data["HMov"] = rawImport["HMov"][0][0]
+
+"""
+7) Split the data by the night vector
+"""
+# Get the recording times for night 1 and 2
+DivDF_Patient = DivDF.loc[DivDF["EDF_ID"] == ID]
+
+Time_N1 = float(DivDF_Patient.iloc[0]["Rec_Time_N1"][0:3])  # in hours
+Time_N2 = float(DivDF_Patient.iloc[0]["Rec_Time_N2"][0:3])  # in hours
+
+# Convert these recording times to seconds then to samples
+sample_n1 = (Time_N1 * 3600) * data["fs"]
+sample_n2 = (Time_N2 * 3600) * data["fs"]
+
+# If there is left over in the middle, take midpoint
+if sample_n2 + sample_n1 < len(data["EEG"]):
+    gap = len(data["EEG"]) - sample_n2 + sample_n1
+
+    # Set sample one to include first half of gap
+    sample_n1 = sample_n1 + (0.5 * gap)
+
+    # Set sample 2 to include samples from middle of gap to end
+    sample_n2 = len(data["EEG"]) - sample_n1
+
+elif sample_n2 + sample_n1 > len(data["EEG"]):
+
+    # Calculate overlap
+    overlap = abs(len(data["EEG"]) - sample_n1 - sample_n2)
+
+    # Set sample one to exclude half of gap
+    sample_n1 = sample_n1 - (0.5 * overlap)
+
+    # Set sample 2 to include samples from middle of overlap to end
+    sample_n2 = len(data["EEG"]) - sample_n1
+
+# Divide the EEG, eeg filtered, art, and beta vectors into night based
+sample_n1 = round(sample_n1)
+
+Data_N1 = {}
+Data_N2 = {}
+
+Data_N1["EEG"] = data["EEG"][0:sample_n1]
+Data_N2["EEG"] = data["EEG"][sample_n1:-1]
+
+Data_N1["EEG_ArtZero"] = data["EEG_ArtZero"][0:sample_n1]
+Data_N2["EEG_ArtZero"] = data["EEG_ArtZero"][sample_n1:-1]
+
+Data_N1["ArtZero"] = data["ArtZero"][0:sample_n1]
+Data_N2["ArtZero"] = data["ArtZero"][sample_n1:-1]
+
+Data_N1["EEG_Beta"] = data["EEG_Beta"][0:sample_n1]
+Data_N2["EEG_Beta"] = data["EEG_Beta"][sample_n1:-1]
+
+Data_N1["EEG_Filt"] = data["EEG_Filt"][0:sample_n1]
+Data_N2["EEG_Filt"] = data["EEG_Filt"][sample_n1:-1]
+
+Data_N1["Beta_Labels"] = data["Beta_Labels"][0:sample_n1]
+Data_N2["Beta_Labels"] = data["Beta_Labels"][sample_n1:-1]
+
+Data_N1["TimeVec"] = data["TimeVec"][0:sample_n1]
+Data_N2["TimeVec"] = data["TimeVec"][sample_n1:-1]
+
+Data_N1["fs"] = data["fs"]
+Data_N2["fs"] = data["fs"]
+
+'''
+YASA sleep scoring (EEG + EOG (LEOG, REOG))
+'''
+
+"""
+8) Yasa spindle detection
+9) Noise spindle elimination
+"""
+# This function detects the spindles and does our artifact based elimination
+Spindles_N1 = my.detect_spindles(Data_N1)
+Spindles_N2 = my.detect_spindles(Data_N2)
+
+'''
+Detection and removal of artifacts by amplitude
+'''
+
+# See in the signal any point that goes above 250uV
+# and convert it to NaN
+"""
+3) Artifact removal (create label vec with artifact locations as Nan)
+"""
+start = 100000
+finish = start + 7680
+
+print("Creating artifact label vector...")
+data["ArtZero"] = np.zeros(len(data["EEG"]))
+for i in range(len(data["ArtZero"])):
+    if abs(data["EEG"][i]) >= 250:
+        data["ArtZero"][i] = np.nan
+
+# EOG Channels artifact removal
+for i in range(len(data["LEOG"])):
+    if abs(data["LEOG"][i]) >= 250:
+        data["LEOG"][i] = 0
+
+for i in range(len(data["REOG"])):
+    if abs(data["REOG"][i]) >= 250:
+        data["REOG"][i] = 0
+
+'''
+Removal of artifacts by arousals (using beta band)
+'''
+
+# Band pass filter (20-50 Hz)
+"""
+4) Band pass filter the EEG data (10-16 Hz)
+"""
+print("Bandpass filtering...")
+data["EEG_Filt"] = my.butter_bandpass_filter(data["EEG"], 10, 16, data["fs"], order=5)
+
+"""
+5) Upper Beta pass filter to graph (20-50 Hz)
+"""
+print("Beta pass filtering...")
+data["EEG_Beta"] = my.butter_bandpass_filter(data["EEG"], 20, 50, data["fs"], order=5)
+data["Beta_Labels"] = np.zeros(len(data["EEG_Beta"]))
+
+# For any threshold event, change the beta label vec to 1
+for i in range(len(data["EEG_Beta"])):
+    if data["EEG_Beta"][i] > 10:
+        data["Beta_Labels"][i - 64:i + 64] = 1
+
+# Root mean square (RMS)
+
+# Mean and STD
+
+# *Everything that is over mean + 2*STD is an artifact 
+# (Zach uses 10 as an approx)
+
+'''
+Select only N2 and N3 of the Sleep Scoring
+'''
+
+# Use NaN for Wake, N1 and REM
+
+'''
+Do PSD (IRASA)
+'''
+
+# All band
+
+# Slow spindles (9 - 12.5 Hz)
+
+# Fast spindles (12.5 - 16 Hz)
+
+# *It may be neccesary to smooth the IRASA curve
